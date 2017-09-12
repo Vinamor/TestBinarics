36c92f3b87ef15bba1637e0138e71d3843bfa438
Binariks <admin@binariks.com>
2017-09-12 17:44:44 +0300

    First commit

---
+++ Desktop/My Apps/GLDemoObjective-C/GLDemoObjective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLDemoObjective-C
+//
+//  Created by Ostap Romaniv on 9/12/17.
+//  Copyright © 2017 Ostap Romaniv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
